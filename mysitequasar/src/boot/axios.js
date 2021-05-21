71d3bcded965a9f910972732792cd68709563e17
Yalçın Can <ylcn58_can@hotmail.com>
2021-05-21 19:10:31 +0300

    Axios js changed

--- mysitequasar/src/boot/axios.js
+++ mysitequasar/src/boot/axios.js
@@ -5,4 +5,11 @@ export default ({ store, Vue }) => {
   Vue.prototype.$axios = axios;
   store.$axios = axios;
   axios.defaults.baseURL = "http://localhost:52951/api/";
+  axios.interceptors.request.use(function(config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  });
 };
