6bd80fe8ee27973c0f28f9d5f7550657899560c0
Yalçın Can <ylcn58_can@hotmail.com>
2021-05-22 13:11:54 +0300

    Exception log aspect updated

--- MySiteBackend/Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
+++ MySiteBackend/Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -39,16 +39,33 @@ namespace Core.Aspects.Autofac.Exception
 
         private string GetLogDetail(IInvocation invocation, System.Exception e)
         {
-            var ex = e.InnerException != null ? (ApiException)e.InnerException : (ApiException)e;
-            var errorlogDetail = new ErrorLog
+           if (e.InnerException is ApiException || e.GetType() == typeof(ApiException))
             {
-                Errors = ex.Errors,
-                MethodName = invocation.Method.Name,
-                ManagerName = invocation.MethodInvocationTarget.ReflectedType.Name,
-                UserId = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value,
-                Username = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value,
-            };
-            return JsonConvert.SerializeObject(errorlogDetail);
+                var ex = e.InnerException != null ? (ApiException)e.InnerException : (ApiException)e;
+                var errorlogDetail = new ErrorLog
+                {
+                    Errors = ex.Errors,
+                    MethodName = invocation.Method.Name,
+                    ManagerName = invocation.MethodInvocationTarget.ReflectedType.Name,
+                    UserId = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value,
+                    Username = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value,
+                };
+                return JsonConvert.SerializeObject(errorlogDetail);
+            }
+            else
+            {
+                List<string> exceptions = new List<string>();
+                exceptions.Add(e.Message);
+                var errorlogDetail = new ErrorLog
+                {
+                    Errors = exceptions,
+                    MethodName = invocation.Method.Name,
+                    ManagerName = invocation.MethodInvocationTarget.ReflectedType.Name,
+                    UserId = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value,
+                    Username = (_httpContextAccessor.HttpContext == null || _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value == null) ? "?" : _httpContextAccessor.HttpContext.User.Claims.First(x => x.Type == ClaimTypes.Name).Value,
+                };
+                return JsonConvert.SerializeObject(errorlogDetail);
+            }
         }
     }
 }
